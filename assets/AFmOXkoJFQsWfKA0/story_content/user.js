1a552b4f1659945c3b1c5fe332a9befa27c68ca9
rchl-m <rachel_montelibano@yahoo.com>
2025-12-31 02:42:24 +0800

    Add files via upload

---
+++ assets/AFmOXkoJFQsWfKA0/story_content/user.js
@@ -0,0 +1,385 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6FGKe0h1tWp":
+        Script1();
+        break;
+      case "6TPpSuf45sI":
+        Script2();
+        break;
+      case "5l9h9z5CGlC":
+        Script3();
+        break;
+      case "5aBA6IAe9f7":
+        Script4();
+        break;
+      case "6AEU42Fiii6":
+        Script5();
+        break;
+      case "6cBKVaPaF6p":
+        Script6();
+        break;
+      case "5mEyVviK0Sc":
+        Script7();
+        break;
+      case "6QwGOW6eHLS":
+        Script8();
+        break;
+      case "5Xrv8KaW1cj":
+        Script9();
+        break;
+      case "6WtnL2gdMU2":
+        Script10();
+        break;
+      case "6EfgLYvhmnf":
+        Script11();
+        break;
+      case "5pJpvRDmL3y":
+        Script12();
+        break;
+      case "6Dk46mUyRvU":
+        Script13();
+        break;
+      case "6oN8HaA8sSM":
+        Script14();
+        break;
+      case "5nut44jYwNI":
+        Script15();
+        break;
+      case "68w68jdmAoW":
+        Script16();
+        break;
+      case "6UWYEjNwAMf":
+        Script17();
+        break;
+      case "6nHYnk5QxeW":
+        Script18();
+        break;
+      case "6ANDIkPnV2B":
+        Script19();
+        break;
+      case "69sv8xccGJm":
+        Script20();
+        break;
+      case "6oMGWH4DzDA":
+        Script21();
+        break;
+  }
+}
+
+function Script1()
+{
+  function thousands_separators(num)
+ {
+ var num_parts = num.toString().split(".");
+ num_parts[0] = num_parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+ return num_parts.join(".");
+ }
+
+var player = GetPlayer();
+var n = player.GetVar("CountCorrect")
+var f = thousands_separators(n);
+player.SetVar("CountCorrectText",f);
+}
+
+function Script2()
+{
+  function thousands_separators(num)
+ {
+ var num_parts = num.toString().split(".");
+ num_parts[0] = num_parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+ return num_parts.join(".");
+ }
+
+var player = GetPlayer();
+var n = player.GetVar("CountCorrect")
+var f = thousands_separators(n);
+player.SetVar("CountCorrectText",f);
+}
+
+function Script3()
+{
+  function thousands_separators(num)
+ {
+ var num_parts = num.toString().split(".");
+ num_parts[0] = num_parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+ return num_parts.join(".");
+ }
+
+var player = GetPlayer();
+var n = player.GetVar("CountCorrect")
+var f = thousands_separators(n);
+player.SetVar("CountCorrectText",f);
+}
+
+function Script4()
+{
+  function thousands_separators(num)
+ {
+ var num_parts = num.toString().split(".");
+ num_parts[0] = num_parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+ return num_parts.join(".");
+ }
+
+var player = GetPlayer();
+var n = player.GetVar("CountCorrect")
+var f = thousands_separators(n);
+player.SetVar("CountCorrectText",f);
+}
+
+function Script5()
+{
+  function thousands_separators(num)
+ {
+ var num_parts = num.toString().split(".");
+ num_parts[0] = num_parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+ return num_parts.join(".");
+ }
+
+var player = GetPlayer();
+var n = player.GetVar("CountCorrect")
+var f = thousands_separators(n);
+player.SetVar("CountCorrectText",f);
+}
+
+function Script6()
+{
+  function thousands_separators(num)
+ {
+ var num_parts = num.toString().split(".");
+ num_parts[0] = num_parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+ return num_parts.join(".");
+ }
+
+var player = GetPlayer();
+var n = player.GetVar("CountCorrect")
+var f = thousands_separators(n);
+player.SetVar("CountCorrectText",f);
+}
+
+function Script7()
+{
+  function thousands_separators(num)
+ {
+ var num_parts = num.toString().split(".");
+ num_parts[0] = num_parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+ return num_parts.join(".");
+ }
+
+var player = GetPlayer();
+var n = player.GetVar("CountCorrect")
+var f = thousands_separators(n);
+player.SetVar("CountCorrectText",f);
+}
+
+function Script8()
+{
+  function thousands_separators(num)
+ {
+ var num_parts = num.toString().split(".");
+ num_parts[0] = num_parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+ return num_parts.join(".");
+ }
+
+var player = GetPlayer();
+var n = player.GetVar("CountCorrect")
+var f = thousands_separators(n);
+player.SetVar("CountCorrectText",f);
+}
+
+function Script9()
+{
+  function thousands_separators(num)
+ {
+ var num_parts = num.toString().split(".");
+ num_parts[0] = num_parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+ return num_parts.join(".");
+ }
+
+var player = GetPlayer();
+var n = player.GetVar("CountCorrect")
+var f = thousands_separators(n);
+player.SetVar("CountCorrectText",f);
+}
+
+function Script10()
+{
+  function thousands_separators(num)
+ {
+ var num_parts = num.toString().split(".");
+ num_parts[0] = num_parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+ return num_parts.join(".");
+ }
+
+var player = GetPlayer();
+var n = player.GetVar("CountCorrect")
+var f = thousands_separators(n);
+player.SetVar("CountCorrectText",f);
+}
+
+function Script11()
+{
+  function thousands_separators(num)
+ {
+ var num_parts = num.toString().split(".");
+ num_parts[0] = num_parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+ return num_parts.join(".");
+ }
+
+var player = GetPlayer();
+var n = player.GetVar("CountCorrect")
+var f = thousands_separators(n);
+player.SetVar("CountCorrectText",f);
+}
+
+function Script12()
+{
+  function thousands_separators(num)
+ {
+ var num_parts = num.toString().split(".");
+ num_parts[0] = num_parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+ return num_parts.join(".");
+ }
+
+var player = GetPlayer();
+var n = player.GetVar("CountCorrect")
+var f = thousands_separators(n);
+player.SetVar("CountCorrectText",f);
+}
+
+function Script13()
+{
+  function thousands_separators(num)
+ {
+ var num_parts = num.toString().split(".");
+ num_parts[0] = num_parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+ return num_parts.join(".");
+ }
+
+var player = GetPlayer();
+var n = player.GetVar("CountCorrect")
+var f = thousands_separators(n);
+player.SetVar("CountCorrectText",f);
+}
+
+function Script14()
+{
+  function thousands_separators(num)
+ {
+ var num_parts = num.toString().split(".");
+ num_parts[0] = num_parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+ return num_parts.join(".");
+ }
+
+var player = GetPlayer();
+var n = player.GetVar("CountCorrect")
+var f = thousands_separators(n);
+player.SetVar("CountCorrectText",f);
+}
+
+function Script15()
+{
+  function thousands_separators(num)
+ {
+ var num_parts = num.toString().split(".");
+ num_parts[0] = num_parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+ return num_parts.join(".");
+ }
+
+var player = GetPlayer();
+var n = player.GetVar("CountCorrect")
+var f = thousands_separators(n);
+player.SetVar("CountCorrectText",f);
+}
+
+function Script16()
+{
+  function thousands_separators(num)
+ {
+ var num_parts = num.toString().split(".");
+ num_parts[0] = num_parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+ return num_parts.join(".");
+ }
+
+var player = GetPlayer();
+var n = player.GetVar("CountCorrect")
+var f = thousands_separators(n);
+player.SetVar("CountCorrectText",f);
+}
+
+function Script17()
+{
+  function thousands_separators(num)
+ {
+ var num_parts = num.toString().split(".");
+ num_parts[0] = num_parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+ return num_parts.join(".");
+ }
+
+var player = GetPlayer();
+var n = player.GetVar("CountCorrect")
+var f = thousands_separators(n);
+player.SetVar("CountCorrectText",f);
+}
+
+function Script18()
+{
+  function thousands_separators(num)
+ {
+ var num_parts = num.toString().split(".");
+ num_parts[0] = num_parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+ return num_parts.join(".");
+ }
+
+var player = GetPlayer();
+var n = player.GetVar("CountCorrect")
+var f = thousands_separators(n);
+player.SetVar("CountCorrectText",f);
+}
+
+function Script19()
+{
+  function thousands_separators(num)
+ {
+ var num_parts = num.toString().split(".");
+ num_parts[0] = num_parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+ return num_parts.join(".");
+ }
+
+var player = GetPlayer();
+var n = player.GetVar("CountCorrect")
+var f = thousands_separators(n);
+player.SetVar("CountCorrectText",f);
+}
+
+function Script20()
+{
+  function thousands_separators(num)
+ {
+ var num_parts = num.toString().split(".");
+ num_parts[0] = num_parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+ return num_parts.join(".");
+ }
+
+var player = GetPlayer();
+var n = player.GetVar("CountCorrect")
+var f = thousands_separators(n);
+player.SetVar("CountCorrectText",f);
+}
+
+function Script21()
+{
+  function thousands_separators(num)
+ {
+ var num_parts = num.toString().split(".");
+ num_parts[0] = num_parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+ return num_parts.join(".");
+ }
+
+var player = GetPlayer();
+var n = player.GetVar("CountCorrect")
+var f = thousands_separators(n);
+player.SetVar("CountCorrectText",f);
+}
+
